cd5d176af75fb10323e47a768498388a319645f2
tugce-duru-aydin <tugceduruaydin@gmail.com>
2026-06-02 11:02:05 +0300

    2.Commit (Tuğçe Duru Aydın)

---
+++ ObjectvilleGame (2).java
@@ -0,0 +1,100 @@
+import java.io.*;
+import java.util.*;
+
+public class ObjectvilleGame {
+    private static final int CAPACITY = 100;
+
+    private static final char EMPTY = 'E';
+    private static final char ROAD = 'R';
+    private static final char HOUSING = 'H';
+    private static final char INDUSTRIAL = 'I';
+    private static final char COMMERCIAL = 'C';
+    private static final char POWER = 'P';
+    private static final char WATER = 'W';
+    private static final char INTERNET = 'T';
+    private static final char SCHOOL = 'S';
+    private static final char POLICE_STATION = 'F';
+    private static final char HOSPITAL = 'D';
+
+    private static final int SCHOOL_RADIUS = 4;
+    private static final int POLICE_RADIUS = 5;
+    private static final int HOSPITAL_RADIUS = 3;
+
+    static class Zone {
+        char type;
+        int level = 0;
+        int row, col;
+
+        int electricity = 0;
+        int water = 0;
+        int internet = 0;
+
+        boolean hasSecurity = false;
+        boolean hasHealth = false;
+        boolean hasEducation = false;
+
+        int populationReceived = 0;
+        int goodsReceived = 0;
+        int lifestyleReceived = 0;
+
+        int prevOutput = 0;
+
+        Zone(int row, int col, char type) {
+            this.row = row;
+            this.col = col;
+            this.type = type;
+        }
+
+        double euclideanDistance(Zone other) {
+            double dRow = other.row - this.row;
+            double dCol = other.col - this.col;
+            return Math.sqrt(dRow * dRow + dCol * dCol);
+        }
+
+        String getKey() {
+            return row + "," + col;
+        }
+    }
+
+    static class ServiceBuilding extends Zone {
+        int radius;
+        ServiceBuilding(int row, int col, char type, int radius) {
+            super(row, col, type);
+            this.radius = radius;
+        }
+    }
+
+    static class UtilityProvider extends Zone {
+        int distribution;
+        UtilityProvider(int row, int col, char type) {
+            super(row, col, type);
+            this.distribution = CAPACITY;
+        }
+    }
+
+    private int rows, cols;
+    private Zone[][] grid;
+
+    private List<Zone> activeZones = new ArrayList<>();
+    private List<ServiceBuilding> services = new ArrayList<>();
+    private List<UtilityProvider> powerPlants = new ArrayList<>();
+    private List<UtilityProvider> waterPumps = new ArrayList<>();
+    private List<UtilityProvider> internetHubs = new ArrayList<>();
+
+    private int totalPopulation = 0;
+    private int totalGoods = 0;
+    private int totalLifestyle = 0;
+
+    private static final List<Character> UTILITY_ORDER = Arrays.asList(INTERNET, WATER, POWER);
+
+    public ObjectvilleGame(String mapFile) throws IOException {
+        loadMap(mapFile);
+    }
+
+    private String getZoneName(Zone zone) {
+        if (zone.type == HOUSING) return "House";
+        if (zone.type == COMMERCIAL) return "Commercial";
+        if (zone.type == INDUSTRIAL) return "Industrial";
+        return String.valueOf(zone.type);
+    }
+    }
